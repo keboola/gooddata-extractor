2ce83fd6118e7e5acf9c44ea45652b28946ed970
Jakub Matějka <jakub@keboola.com>
2020-02-18 17:55:06 +0100

    feat: add verbose logging

--- src/Keboola/GoodDataExtractor/Extractor.php
+++ src/Keboola/GoodDataExtractor/Extractor.php
@@ -6,6 +6,7 @@ namespace Keboola\GoodDataExtractor;
 
 use Keboola\GoodData\Client;
 use Keboola\GoodData\Exception as GoodDataException;
+use Psr\Log\LoggerInterface;
 
 class Extractor
 {
@@ -13,12 +14,26 @@ class Extractor
     protected $gdClient;
     /** @var string  */
     protected $folder;
+    /** @var LoggerInterface */
+    protected $logger;
 
     public function __construct(Client $gdClient, string $username, string $password, string $folder)
     {
         $this->gdClient = $gdClient;
         $this->gdClient->login($username, $password);
         $this->folder = $folder;
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+
+    protected function log(string $message): void
+    {
+        if (!empty($this->logger)) {
+            $this->logger->info($message);
+        }
     }
 
     public function extract(array $reports): void
@@ -38,15 +53,18 @@ class Extractor
     public function download(string $pid, string $uri, string $filename): void
     {
         try {
+            $this->log("Downloading report '{$uri}'");
             $report = $this->gdClient->get($uri);
             if (!isset($report['report']['content']['definitions'][0])) {
                 throw new Exception("Report '{$uri}' has no definitions to export");
             }
             $reportDefinitions = $report['report']['content']['definitions'];
             $reportDefinitionUri = array_pop($reportDefinitions);
+            $this->log("Found report definition '{$reportDefinitionUri}'");
 
             $responseUri = $this->gdClient->getReports()->export($pid, $reportDefinitionUri);
 
+            $this->log("Downloading report data from '{$responseUri}'");
             $this->gdClient->getToFile($responseUri, $filename);
         } catch (GoodDataException $e) {
             throw new Exception($e->getMessage(), 400, $e);
